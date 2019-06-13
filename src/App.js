6f2226137c0ef691988744b99092e3bb010d33b5
Jacob Lam <jlam18@binghamton.edu>
2019-06-13 09:04:12 -0400

    able to fetch data from backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component{
 
-    </div>
-  );
+  state = {
+    stores: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/')
+      .then( res => res.json() )
+      .then( stores => {
+        this.setState({
+          stores:stores.stores
+        })
+      })
+  }
+
+  render(){
+    console.log(this.state)
+    return (
+      <div className="App">
+        i think this works
+      </div>
+    );
+  }
+
 }
-
 export default App;
