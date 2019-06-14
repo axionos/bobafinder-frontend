ebc2e48ffebe3f70d7573ba0091fdbafb469843d
Jacob Lam <jlam18@binghamton.edu>
2019-06-14 09:22:25 -0400

    working react routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import LoginPage from './LoginPage'
+import SignupPage from './SignupPage'
 
 class App extends React.Component{
 
   state = {
     stores: [],
-    page: "login"
   }
 
   componentDidMount(){
-    fetch('http://localhost:3000/')
-      .then( res => res.json() )
-      .then( stores => {
-        this.setState({
-          stores:stores.stores
-        })
-      })
+    // fetch('http://localhost:3000/')
+    //   .then( res => res.json() )
+    //   .then(data => {
+    //     this.setState({
+    //       stores: data.stores
+    //     }, () => (console.log(this.state)))
+    //   })
   }
 
-  redirect = (page) => {
-    this.setState({
-      page: page
-    })
-  }
-
+  // <div className="App">
+  // <LoginPage />
+  // </div>
   render(){
     console.log(this.state)
     return (
       <div className="App">
-        <LoginPage redirect={this.redirect}/>
+
       </div>
-    );
+    )
   }
 
 }
