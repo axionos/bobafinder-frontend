53f093a3154b38fb2247a147f2ee83d1c00ee688
Jacob Lam <jlam18@binghamton.edu>
2019-06-14 09:49:05 -0400

    manual clean up

---
+++ src/components/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class SignUp extends React.Component{
+
+
+  render(){
+
+    return (
+      <div className="signupPage">
+        hello from SignUp Page!
+      </div>
+    );
+  }
+
+}
+export default SignUp;
