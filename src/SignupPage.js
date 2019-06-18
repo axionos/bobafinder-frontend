d8904006b9f8d342f414be3e0c5d7e7d434641a1
axionos <axionoso@gmail.com>
2019-06-18 16:46:38 -0400

    style signup page

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Form, Button} from 'react-bootstrap'
+import './form.css'
 
 class SignupPage extends React.Component{
 
@@ -7,7 +9,14 @@ class SignupPage extends React.Component{
     password: ''
 
   }
-
+  // <h3>Sign up form</h3>
+  // <form onSubmit={this.handleSignUp}>
+  //   Username
+  //   <input type='text' name="username" onChange={this.handleChange}/>
+  //   Password
+  //   <input type='password' name="password" onChange={this.handleChange} />
+  //   <input type='submit' value="Sign Up" />
+  // </form>
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value
@@ -38,15 +47,28 @@ class SignupPage extends React.Component{
 
   render(){
     return(
-      <div>
-        <h3>Sign up form</h3>
-        <form onSubmit={this.handleSignUp}>
-          Username
-          <input type='text' name="username" onChange={this.handleChange}/>
-          Password
-          <input type='password' name="password" onChange={this.handleChange} />
-          <input type='submit' value="Sign Up" />
-        </form>
+      <div className="wrapper">
+        <div className="signup-container">
+          <div className="signup-wrapper container">
+            <h3>Join <span className="join">Boba Finder! 😋</span></h3>
+            <Form onSubmit={this.handleSignUp}>
+              <Form.Group controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Enter Username" onChange={this.handleChange}/>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" onChange={this.handleChange}/>
+              </Form.Group>
+              <div className="signup-btn">
+                <Button className="signup" variant="primary" type="submit" value="Sign Up">
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
       </div>
     )
   }
